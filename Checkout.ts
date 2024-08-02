8238d67573ded6c9311da84d9f15f827763e6832
Ragini0927 <92200080+Ragini0927@users.noreply.github.com>
2024-08-02 11:55:15 +0530

    Update Checkout.ts

--- Checkout.ts
+++ Checkout.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-checkout',
@@ -7,28 +6,18 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./checkout.component.css']
 })
 export class CheckoutComponent {
-  checkoutForm: FormGroup;
+  checkoutData = {
+    name: '',
+    email: '',
+    address: ''
+  };
 
-  constructor(private fb: FormBuilder) {
-    this.checkoutForm = this.fb.group({
-      firstName: ['', Validators.required],
-      lastName: ['', Validators.required],
-      username: ['', Validators.required],
-      email: ['', [Validators.email]],
-      address: ['', Validators.required],
-      address2: [''],
-      country: ['', Validators.required],
-      state: ['', Validators.required],
-      zip: ['', Validators.required],
-      sameAddress: [false],
-      saveInfo: [false],
-      promoCode: ['']
-    });
-  }
-
-  onSubmit() {
-    if (this.checkoutForm.valid) {
-      console.log('Form Submitted!', this.checkoutForm.value);
+  onSubmit(form) {
+    if (form.valid) {
+      console.log('Form Submitted', this.checkoutData);
+      // Perform checkout logic, like calling an API
+    } else {
+      console.log('Form is invalid');
     }
   }
 }
